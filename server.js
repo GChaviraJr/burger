3c4181d805eb6c3d5098ddee44b4600c81f18288
Lupe <lupec_se@yahoo.com>
2019-01-20 21:59:55 -0700

    added depencies handlebar files finishes and server finish.

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 const app = express()
-const PORT = process.env.PORT || 3000
+//Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + '/public'))
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// Handlebars
+const exphbs = require('express-handlebars')
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
 
 
+const router = require('./controllers/burgers_controllers.js')
+app.use('/', router)
 
-
-
-app.listen(PORT, function() {
+// Open Server
+const port = process.env.PORT || 3000
+app.listen(port, () => {
     // Log (server-side) when our server has started
     console.log("App is listening on" + PORT);
   });
