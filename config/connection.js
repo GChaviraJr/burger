298327ad97637d2aa851d8e9cd307c845cca539f
Lupe <lupec_se@yahoo.com>
2019-01-27 21:49:02 -0700

    minor changes

--- config/connection.js
+++ config/connection.js
@@ -15,13 +15,13 @@ connection = mysql.createConnection({
   
 
 
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return
-  }
-  console.log('connected as id ' + connection.threadId);
-})
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error('error connecting: ' + err.stack);
+//     return
+//   }
+//   console.log('connected as id ' + connection.threadId);
+// })
 
 
 module.exports = connection
