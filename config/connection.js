bd8b72303bda3a7f87b4241637446d4520e7291c
Lupe <lupec_se@yahoo.com>
2019-01-27 20:56:03 -0700

    additional minor changes.

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,14 @@
 const mysql = require('mysql')
 
-const connection = mysql.createConnection({
+const connection;
+
+if (process.env.JAWSDB_URL) {
+  conection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+
+}
+  connection = mysql.createConnection({
   host: 'localhost',
-  port: 3306,
   user: 'root',
   password: 'Sb1598951',
   database: 'burgers_db'
